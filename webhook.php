d94282d866a6df86220b61f1cfe65a3546c81373
PranavMindtree <pranav.pathak@mindtree.com>
2016-07-14 16:33:36 +0530

    inital files added

---
+++ webhook.php
@@ -0,0 +1,20 @@
+<?php
+ $challenge = $_REQUEST['hub_challenge'];
+ $verify_token = $_REQUEST['hub_verify_token'];
+ if ($verify_token == 'abc123') {echo $challenge;}
+
+ //you can output the below to your error log and tail -f it to see them
+ //feed in live, once you see them hit your error log you know your
+ //listener works, then you can change the below code to handle the
+ //array and grab the leadgen ID for a further GET request of that real-time entry;
+
+  $input = json_decode(file_get_contents('php://input'), true);
+  error_log(print_r($input, true));
+  $name = mktime();
+  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+  $txt = print_r($input, true);
+  fwrite($myfile, $txt);
+  fclose($myfile);
+ 
+ 
+ ?>
