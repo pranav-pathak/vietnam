748ffdc6909a4e47e8635fcbf5bdf1bf20824504
pranavpathak <pranavpathak125@gmail.com>
2016-07-15 23:02:40 +0530

    composer run

--- myfile.php
+++ myfile.php
@@ -6,18 +6,24 @@ $myfile = fopen("leadgen.txt", "r") or die("Unable to open file!");
 echo $leadgen_id =  fread($myfile,filesize("leadgen.txt"));
 fclose($myfile);
 
-$fb = new Facebook\Facebook([
-  'app_id' => '638113826341564',
-  'app_secret' => '8f5d4971c70fe677c9c9b2c44e74aa9b',
-  'default_graph_version' => 'v2.5',
-  ]);
-print_r($fb);
+
 use FacebookAds\Object\Lead;
 
-$form = new Lead($leadgen_id);
+$form = new Lead('1043952978973660');
 $form->read();
 
-print_r($form);
+// $fb = new Facebook\Facebook([
+  // 'app_id' => '638113826341564',
+  // 'app_secret' => '8f5d4971c70fe677c9c9b2c44e74aa9b',
+  // 'default_graph_version' => 'v2.5',
+  // ]);
+// print_r($fb);
+// use FacebookAds\Object\Lead;
+
+// $form = new Lead($leadgen_id);
+// $form->read();
+
+// print_r($form);
 
 
 
