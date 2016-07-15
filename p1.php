b4c5612d93f2b64603c7bd155f47e6189c880685
PranavMindtree <pranav.pathak@mindtree.com>
2016-07-15 17:12:07 +0530

    1inital files added

---
+++ p1.php
@@ -0,0 +1,52 @@
+<?php
+
+session_start();
+ 
+require_once( 'Facebook/FacebookSession.php' );
+require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'Facebook/FacebookRequest.php' );
+require_once( 'Facebook/FacebookResponse.php' );
+require_once( 'Facebook/FacebookSDKException.php' );
+require_once( 'Facebook/FacebookRequestException.php' );
+require_once( 'Facebook/FacebookAuthorizationException.php' );
+require_once( 'Facebook/GraphObject.php' );
+ 
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+ 
+// init app with app id (APPID) and secret (SECRET)
+FacebookSession::setDefaultApplication(638113826341564,8f5d4971c70fe677c9c9b2c44e74aa9b);
+ 
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper( 'https://vietnam-test-server.herokuapp.com' );
+ 
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
+}
+ 
+// see if we have a session
+if ( isset( $session ) ) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+   
+  // print data
+  echo  print_r( $graphObject, 1 );
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
+}
+
+?>
