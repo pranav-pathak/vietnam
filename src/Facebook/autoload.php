7e608a02a6332c466a7e895095d53a9f2d3508f8
PranavMindtree <pranav.pathak@mindtree.com>
2016-07-15 17:45:46 +0530

    1inital files added

--- src/Facebook/autoload.php
+++ src/Facebook/autoload.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2014 Facebook, Inc.
+ * Copyright 2016 Facebook, Inc.
  *
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
@@ -27,6 +27,7 @@
  * Why are you not using composer?
  * https://getcomposer.org/
  */
+
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
     throw new Exception('The Facebook SDK requires PHP version 5.4 or higher.');
 }
