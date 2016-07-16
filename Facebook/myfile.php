90bc7875183564a59c350e64f035439fff8d40e7
pranavpathak <pranavpathak125@gmail.com>
2016-07-16 14:35:53 +0530

    code update

--- Facebook/myfile.php
+++ Facebook/myfile.php
@@ -1,21 +1,36 @@
 <?php
 
-// $myfile = fopen("newfile1.txt", "w+") or die("Unable to open file!");
+$myfile = fopen("newfile1.txt", "w+") or die("Unable to open file!");
 
-// $myfile = fopen("leadgen.txt", "r") or die("Unable to open file!");
-// echo $leadgen_id =  fread($myfile,filesize("leadgen.txt"));
-// fclose($myfile);
+$myfile = fopen("leadgen.txt", "r") or die("Unable to open file!");
+echo $leadgen_id =  fread($myfile,filesize("leadgen.txt"));
+fclose($myfile);
 
-$fb = new Facebook\Facebook([
-  'app_id' => '638113826341564',
-  'app_secret' => '8f5d4971c70fe677c9c9b2c44e74aa9b',
-  'default_graph_version' => 'v2.5',
-]);
-$response = $fb->get('/me');
+// $fb = new Facebook\Facebook([
+  // 'app_id' => '638113826341564',
+  // 'app_secret' => '8f5d4971c70fe677c9c9b2c44e74aa9b',
+  // 'default_graph_version' => 'v2.5',
+// ]);
+
+use FacebookAds\Api;
+
+$app_id ='638113826341564';
+$app_secret='8f5d4971c70fe677c9c9b2c44e74aa9b'; 
+$access_token='638113826341564|JwPbpCKEGQpo4Mt8t47LlbpihzA';
+// Initialize a new Session and instanciate an Api object
+Api::init($app_id, $app_secret, $access_token);
+
+// The Api object is now available trough singleton
+$api = Api::instance();
+
+echo"api";
+print_r($api);
+echo "<br/>";
+// $response = $fb->get('/me');
 
 use FacebookAds\Object\Lead;
 
-$form = new Lead('1043973408971617');
+$form = new Lead($leadgen_id);
 $form->read();
 
 
--- myfile.php
+++ myfile.php
@@ -6,12 +6,20 @@ $myfile = fopen("leadgen.txt", "r") or die("Unable to open file!");
 echo $leadgen_id =  fread($myfile,filesize("leadgen.txt"));
 fclose($myfile);
 
-// $fb = new Facebook\Facebook([
-  // 'app_id' => '638113826341564',
-  // 'app_secret' => '8f5d4971c70fe677c9c9b2c44e74aa9b',
-  // 'default_graph_version' => 'v2.5',
-// ]);
-// $response = $fb->get('/me');
+use FacebookAds\Api;
+
+$app_id ='638113826341564';
+$app_secret='8f5d4971c70fe677c9c9b2c44e74aa9b'; 
+$access_token='638113826341564|JwPbpCKEGQpo4Mt8t47LlbpihzA';
+// Initialize a new Session and instanciate an Api object
+Api::init($app_id, $app_secret, $access_token);
+
+// The Api object is now available trough singleton
+$api = Api::instance();
+
+echo"api";
+print_r($api);
+echo "<br/>";
 
 use FacebookAds\Object\Lead;
 
