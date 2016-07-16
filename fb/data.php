4c927644f0638db7f4fb2e114df914cce45d9319
pranavpathak <pranavpathak125@gmail.com>
2016-07-16 23:20:59 +0530

    code added

---
+++ fb/data.php
@@ -0,0 +1,36 @@
+<?php
+// require_once( '/vendor/autoload.php' );
+require_once __DIR__ . '/vendor/autoload.php';
+
+// $myfile = fopen("newfile1.txt", "w+") or die("Unable to open file!");
+
+// $myfile = fopen("leadgen.txt", "r") or die("Unable to open file!");
+// echo $leadgen_id =  fread($myfile,filesize("leadgen.txt"));
+// fclose($myfile);
+
+use FacebookAds\Api;
+
+$app_id ='638113826341564';
+$app_secret='8f5d4971c70fe677c9c9b2c44e74aa9b'; 
+$access_token='638113826341564|JwPbpCKEGQpo4Mt8t47LlbpihzA';
+// Initialize a new Session and instanciate an Api object
+Api::init($app_id, $app_secret, $access_token);
+
+// // The Api object is now available trough singleton
+$api = Api::instance();
+
+// echo"api";
+// print_r($api);
+// echo "<br/>";
+
+use Facebook\Object\Lead;
+
+$form = new Lead($leadgen_id);
+$form->read();
+
+
+// use FacebookAds\Api;
+
+// $accounts = Api::instance()->call('/me/accounts');
+
+?>
