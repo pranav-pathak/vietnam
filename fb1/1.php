8a02dff36f897cebb2c455b9e659041ac743209a
pranavpathak <pranavpathak125@gmail.com>
2016-07-16 23:53:26 +0530

    code update

---
+++ fb1/1.php
@@ -0,0 +1,16 @@
+<?php
+
+use FacebookAds\Api;
+
+// Initialize a new Session and instanciate an Api object
+$app_id = "638113826341564";
+$app_secret = "8f5d4971c70fe677c9c9b2c44e74aa9b";
+$access_token = "638113826341564|JwPbpCKEGQpo4Mt8t47LlbpihzA";
+Api::init($app_id, $app_secret, $access_token);
+
+// The Api object is now available trough singleton
+$api = Api::instance();
+
+echo"api";
+print_r($api);
+echo "<br/>";
---
+++ fb1/pp1.php
@@ -0,0 +1,3 @@
+<?php
+define('VENDOR_DIR', '/path/to/sdk/'); // Path to the Vendor directory 
+$loader = include VENDOR_DIR.'/vendor/autoload.php';
