31dc3210016b34627ad46ab07776d95b96578526
pranavpathak <pranavpathak125@gmail.com>
2016-07-18 11:35:16 +0530

    test2

--- test2/1.php
+++ test2/1.php
@@ -2,6 +2,16 @@
 
 use FacebookAds\Api;
 
+
+include('/vendor/autoload.php');
+require __DIR__ . '/src/FacebookAds/Api.php';
+require __DIR__ . '/src/FacebookAds/ApiConfig.php';
+require __DIR__ . '/src/FacebookAds/ApiRequest.php';
+require __DIR__ . '/src/FacebookAds/Cursor.php';
+require __DIR__ . '/src/FacebookAds/Session.php';
+require __DIR__ . '/src/FacebookAds/TypeChecker.php';
+
+
 $app_id = '638113826341564';
 $app_secret = '8f5d4971c70fe677c9c9b2c44e74aa9b';
 $access_token = '638113826341564|JwPbpCKEGQpo4Mt8t47LlbpihzA';
