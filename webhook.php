2bde25112fb2230f3759972629f982e3221d975e
pranavpathak <pranavpathak125@gmail.com>
2016-07-24 19:13:25 +0530

    condition added

--- webhook.php
+++ webhook.php
@@ -20,23 +20,17 @@ $access_token ='EAAJEXHPyArwBAPlUz0JiQL2da5Fz6roaVT8mtASOTF4Bi14p30kWzEqcVNac2Pa
   
  //for user access token goto https://developers.facebook.com/tools/accesstoken/ and click on user access token for your apropriate app section and then in next page clcik on Extend access token, bottom (https://developers.facebook.com/tools/debug/accesstoken?q=EAAJEXHPyArwBACs0kLDEanSLALLk3jGTPhm497VZC81mR6chMTkZCG2RDWilK0s1RLssZCdz2bPy1xbMmJED87ZCjmxxqcHKUZA5UTbMTZBSm0VVN9YC9teaIypyc8pAThGOsP5XHvAAP0ZCLpj2ZBDxdCZCnSzF4lFrl3lztzr9uUAZDZD)
  
-if($input['entry'][0]['changes'][0]['value']['form_id'] == "1042674762487296"){
+// if($input['entry'][0]['changes'][0]['value']['form_id'] == "1042674762487296"){
   $data = getLead($leadgen_id, $access_token);
   fwrite($myfile,'\n lead_data : '. print_r($data, true));
-
-// $lead_str = '[{"full_name": "<test lead: dummy data for full_name>",	"email": "test@fb.com",	"street_address": "<test lead: dummy data for street_address>",	"city": "<test lead: dummy data for city>",	"phone_number": "<test lead: dummy data for phone_number>"}]';
-// $json_arr = json_decode($lead_str);
-// //print_r($json_obj[0]);
 $json_obj = json_encode($data);
-
-
 $ch = curl_init('http://global.enfa.local/wh_post.php');
-$ch = curl_init('http://vietnam.test4.meadjohnson.net/webhook/facebook_lead_ads?t='.time());                                                                      
+$ch = curl_init('http://vietnam.test4.meadjohnson.net/webhook/facebook_lead_ads?t='.time());                                                                     
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);  
 curl_setopt($ch, CURLOPT_HEADER, 0);                                                                
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                         
     'Content-Type: application/json',                                                                                
     'Content-Length: ' . strlen($json_obj))                                                                       
 );
@@ -59,7 +53,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $result = curl_exec($ch);
 fwrite($myfile, print_r($result, true));
 fclose($myfile);  
-}
+// }
 function getLead($leadgen_id,$user_access_token) {
     //fetch lead info from FB API
     $graph_url= 'https://graph.facebook.com/v2.5/'.$leadgen_id."?access_token=".$user_access_token;
